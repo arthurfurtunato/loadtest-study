4f8fc53ddd92cee3ff392832108ba956f29da6fb
arthurfurtunato <arthurfurtunato@gmail.com>
2022-09-14 17:45:14 -0300

    Loadtests in 3 differntes stages: Simple-api, Cluster, and Cluster with Workers dying

---
+++ cluster-api/src/server.js
@@ -0,0 +1,27 @@
+import http from 'http'
+
+const processId = process.pid
+
+const server = http.createServer((request, response) => {
+  for (let index = 0; index< 1e7; index++) {
+  }
+
+  response.end(`Handled by pid: ${processId}`)
+})
+
+server.listen(3000)
+.once('listening', () => {
+  console.log('Server started in process', processId)
+})
+
+// Aguardar as conexões serem encerradas para só então encerrar o programa
+
+process.on('SIGTERM', () => {
+  console.log('server ending', new Date().toISOString())
+  server.close(() => process.exit())
+})
+
+// Vamos simular que um erro aleatorio aconteceu
+setTimeout(() => {
+  process.exit(1)
+}, Math.random() * 1e4)
